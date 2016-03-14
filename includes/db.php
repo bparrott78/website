334b6725e97a07de6d65b9787a2a394300d1abf1
Braeden Parrott <braeden@Braedens-iMac.local>
2016-03-14 17:54:23 -0600

    trying again

--- includes/db.php
+++ includes/db.php
@@ -5,7 +5,7 @@
     $db_name='cms';
     $connection=mysqli_connect($db_host,$db_user,$db_pass,$db_name);
     if (!$db_connect){
-        echo "cannot connect to database" . mysqli_error();
+        echo "cannot connect to database" . mysqli_error($connection);
     }
 
 	
